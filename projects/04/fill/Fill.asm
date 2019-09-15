bd0d11f09a37157ec1ef4ead2e08b438a9f08cd3
Yun <yun26603020@gmail.com>
2019-09-15 04:21:07 +0100

    Project 4: Complete

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,55 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+(PRELOOP)
+
+@R1         // Use R1 as the address offset for the screen and set to 0
+M=0
+
+(LOOP)
+
+@R1         // Check if offset is larger than screen range
+D=M         
+@8192       
+D=A-D
+@PRELOOP
+D;JLE       // Jump to PRELOOP if offset is larger than 8192
+
+@SCREEN     // Start Address of the screen
+D=A
+@R1
+D=D+M
+@pix_addr   // keep pixel address
+M=D         
+
+@KBD        // Check keyboard input
+D=M         
+@BLACK
+D;JNE       // Jump to BLACK if any key in pressed
+
+(WHITE)
+
+@pix_addr  // Make screen white at pix_addr
+A=M
+M=0
+@INC
+0;JMP
+
+(BLACK)     // Make screen black at pix_addr
+
+@pix_addr
+A=M
+M=-1
+@INC
+0;JMP
+
+(INC)
+
+@1         // Increment offset by 16
+D=A
+@R1
+M=D+M
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@1          // Set A to value 1
+D=A         // D = 1
+@i          // Set A to i address
+M=D         // i = 1
+
+(LOOP)
+
+@i          // Set A to i address
+D=M         // D = i
+@R1         // Set A to R1 address
+D=D-M       // D = D - R1
+@END        // Set A to END address
+D;JGT       // Jump to END if D is greater than 0
+@R0         // Set A to R0 address
+D=M         // D = R0
+@product    // Set A to product address
+M=D+M       // product = product + R0
+@i          // Set A to i address
+M=M+1       // i = i + 1
+@LOOP       // Set A to LOOP address
+0;JMP       // Jump to start of LOOP
+
+(END)
+@product    // Set A to product address
+D=M         // D = product
+@R2         // Set A to R2 address
+M=D         // R2 = product
+
+@0          // Set A to 0
+D=A         // D = 0
+@product    // set Memory[product] and Memory[i] back to 0
+M=D
+@i
+M=D
